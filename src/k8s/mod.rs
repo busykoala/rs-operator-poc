7df39d497889c3c6d3255b5ae513ce86727ba9c3
Matthias Osswald <matthias@busykoala.io>
2023-05-10 11:56:25 +0200

    Init most common resources to api.

--- src/k8s/mod.rs
+++ src/k8s/mod.rs
@@ -1,19 +1,35 @@
 use crate::config;
 use crate::k8s;
-use k8s_openapi::api::apps::v1::Deployment;
-use k8s_openapi::api::core::v1::Service;
+use k8s_openapi::api::apps::v1::{Deployment, StatefulSet};
+use k8s_openapi::api::core::v1::{
+    Service, ConfigMap, Secret, PersistentVolumeClaim
+};
+use k8s_openapi::api::networking::v1::Ingress;
+use k8s_openapi::api::batch::v1::CronJob;
 use kube::{Api, Client, config::Config};
 
 pub struct K8sApis {
+    pub config_map: Api<ConfigMap>,
+    pub cron_job: Api<CronJob>,
     pub deployment: Api<Deployment>,
+    pub ingress: Api<Ingress>,
+    pub pvc: Api<PersistentVolumeClaim>,
+    pub secret: Api<Secret>,
     pub service: Api<Service>,
+    pub stateful_set: Api<StatefulSet>,
 }
 
 pub fn get_apis() -> K8sApis {
     let env = config::get_environment();
     return K8sApis {
+        config_map: Api::namespaced(k8s::get_client(), &env.namespace),
+        cron_job: Api::namespaced(k8s::get_client(), &env.namespace),
         deployment: Api::namespaced(k8s::get_client(), &env.namespace),
-        service: Api::namespaced(k8s::get_client(), &env.namespace)
+        ingress: Api::namespaced(k8s::get_client(), &env.namespace),
+        pvc: Api::namespaced(k8s::get_client(), &env.namespace),
+        secret: Api::namespaced(k8s::get_client(), &env.namespace),
+        service: Api::namespaced(k8s::get_client(), &env.namespace),
+        stateful_set: Api::namespaced(k8s::get_client(), &env.namespace),
     }
 }
 
